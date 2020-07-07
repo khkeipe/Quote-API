b71d16ad712a5c0ccdf761b06aaad4ce4e212309
khkeipe <kkeipe@gmail.com>
2020-07-07 14:44:16 -0500

    adding user role enum

---
+++ src/main/java/com/khkeipe/poolQuote/entities/UserRole.java
@@ -0,0 +1,26 @@
+package com.khkeipe.poolQuote.entities;
+
+public enum UserRole {
+
+    ADMIN("Admin"), USER("User"), LOCKED("Locked");
+
+    private final String roleName;
+
+    UserRole(String name){
+        this.roleName = name;
+    }
+
+    public static UserRole getByName(String name) {
+        for(UserRole role : UserRole.values()) {
+            if(role.roleName.equals(name)) {
+                return role;
+            }
+        }
+        return LOCKED;
+    }
+
+    @Override
+    public String toString() {
+        return roleName;
+    }
+}
