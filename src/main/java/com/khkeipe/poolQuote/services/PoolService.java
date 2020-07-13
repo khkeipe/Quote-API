9dd6f5f768581939eafcffeee40e2971d081c62c
khkeipe <kkeipe@gmail.com>
2020-07-13 09:46:43 -0500

    adding get all endpoint for pools

--- src/main/java/com/khkeipe/poolQuote/services/PoolService.java
+++ src/main/java/com/khkeipe/poolQuote/services/PoolService.java
@@ -1,8 +1,14 @@
 package com.khkeipe.poolQuote.services;
 
+import com.khkeipe.poolQuote.entities.Pool;
+import com.khkeipe.poolQuote.exceptions.NotFoundException;
+import com.khkeipe.poolQuote.exceptions.ServerErrorException;
 import com.khkeipe.poolQuote.repositories.PoolRepository;
+import com.khkeipe.poolQuote.util.GetList;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class PoolService {
@@ -14,5 +20,19 @@ public class PoolService {
         this.poolRepo = repo;
     }
 
+    public List<Pool> getAllPools() {
+        Iterable<Pool> e;
+        List<Pool> pools;
+        try {
+            e = poolRepo.findAll();
+            pools = GetList.getListFromIterable(e);
+        } catch (Exception exception) {
+            throw new ServerErrorException();
+        }
+        if(pools.isEmpty()) {
+            throw new NotFoundException("No pools were found");
+        }
+        return pools;
+    }
 
 }
--- src/main/java/com/khkeipe/poolQuote/web/controllers/PoolController.java
+++ src/main/java/com/khkeipe/poolQuote/web/controllers/PoolController.java
@@ -1,9 +1,13 @@
 package com.khkeipe.poolQuote.web.controllers;
 
+import com.khkeipe.poolQuote.entities.Pool;
 import com.khkeipe.poolQuote.services.PoolService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/pools")
@@ -14,5 +18,10 @@ public class PoolController {
     @Autowired
     public PoolController(PoolService service) {
         this.poolService = service;
+    }
+
+    @GetMapping("")
+    public List<Pool> getAllPools() {
+        return poolService.getAllPools();
     }
 }
