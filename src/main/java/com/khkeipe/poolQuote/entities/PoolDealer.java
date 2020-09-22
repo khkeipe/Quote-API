544b8924c157942ffc28f578dc5f7357e9d2c714
Wagenheim <wagenheimk@me.com>
2020-09-22 18:43:04 -0400

    changed up the dealer obj to include entities in the quote obj

--- src/main/java/com/khkeipe/poolQuote/entities/PoolDealer.java
+++ src/main/java/com/khkeipe/poolQuote/entities/PoolDealer.java
@@ -17,18 +17,33 @@ public class PoolDealer {
     @Column
     private String dealerCode;
 
+    @Column
+    private String phoneNumber;
+
+    @Column
+    private String email;
+
+    //NEEDS ANNOTATIONS
+    private Address address;
+
     public PoolDealer() {
     }
 
-    public PoolDealer(String dealerName, String dealerCode) {
+    public PoolDealer(String dealerName, String dealerCode, String phoneNumber, String email, Address address) {
         this.dealerName = dealerName;
         this.dealerCode = dealerCode;
+        this.phoneNumber = phoneNumber;
+        this.email = email;
+        this.address = address;
     }
 
-    public PoolDealer(int id, String dealerName, String dealerCode) {
+    public PoolDealer(int id, String dealerName, String dealerCode, String phoneNumber, String email, Address address) {
         this.id = id;
         this.dealerName = dealerName;
         this.dealerCode = dealerCode;
+        this.phoneNumber = phoneNumber;
+        this.email = email;
+        this.address = address;
     }
 
     public int getId() {
@@ -55,6 +70,30 @@ public class PoolDealer {
         this.dealerCode = dealerCode;
     }
 
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public Address getAddress() {
+        return address;
+    }
+
+    public void setAddress(Address address) {
+        this.address = address;
+    }
+
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
@@ -62,12 +101,15 @@ public class PoolDealer {
         PoolDealer that = (PoolDealer) o;
         return id == that.id &&
                 Objects.equals(dealerName, that.dealerName) &&
-                Objects.equals(dealerCode, that.dealerCode);
+                Objects.equals(dealerCode, that.dealerCode) &&
+                Objects.equals(phoneNumber, that.phoneNumber) &&
+                Objects.equals(email, that.email) &&
+                Objects.equals(address, that.address);
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(id, dealerName, dealerCode);
+        return Objects.hash(id, dealerName, dealerCode, phoneNumber, email, address);
     }
 
     @Override
@@ -76,6 +118,10 @@ public class PoolDealer {
                 "id=" + id +
                 ", dealerName='" + dealerName + '\'' +
                 ", dealerCode='" + dealerCode + '\'' +
+                ", phoneNumber='" + phoneNumber + '\'' +
+                ", email='" + email + '\'' +
+                ", address=" + address +
                 '}';
     }
+
 }
