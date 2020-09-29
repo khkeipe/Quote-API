375714a59cc6bbe714d1c5eeaca5717752cacc3c
Wagenheim <wagenheimk@me.com>
2020-09-28 22:34:10 -0400

    added note

--- src/main/java/com/khkeipe/poolQuote/entities/Quote.java
+++ src/main/java/com/khkeipe/poolQuote/entities/Quote.java
@@ -1,6 +1,7 @@
 package com.khkeipe.poolQuote.entities;
 
 import javax.persistence.*;
+import java.util.List;
 import java.util.Objects;
 
 @Entity
@@ -11,8 +12,10 @@ public class Quote {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
 
+    @Column
     private int orderNumber;
 
+    @Column
     private String requestDate;
 
     @OneToOne(mappedBy = "quote", fetch = FetchType.EAGER, orphanRemoval = true)
@@ -22,16 +25,100 @@ public class Quote {
     @JoinColumn
     private PoolDealer quoteDealer;
 
-    //private Pool quotePool;
+    @OneToMany(mappedBy = "")
+    private List<Pool> quotePool;
+
+    //USER
 
     public Quote() {
     }
 
-    public Quote(int id, int orderNumber, String requestDate, Customer quoteCustomer) {
+    public Quote(int id, int orderNumber, String requestDate, Customer quoteCustomer, PoolDealer quoteDealer) {
         this.id = id;
         this.orderNumber = orderNumber;
         this.requestDate = requestDate;
         this.quoteCustomer = quoteCustomer;
+        this.quoteDealer = quoteDealer;
+    }
+
+    public Quote(int orderNumber, String requestDate, Customer quoteCustomer, PoolDealer quoteDealer) {
+        this.orderNumber = orderNumber;
+        this.requestDate = requestDate;
+        this.quoteCustomer = quoteCustomer;
+        this.quoteDealer = quoteDealer;
+    }
+
+    public Quote(int orderNumber, String requestDate) {
+        this.orderNumber = orderNumber;
+        this.requestDate = requestDate;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getOrderNumber() {
+        return orderNumber;
+    }
+
+    public void setOrderNumber(int orderNumber) {
+        this.orderNumber = orderNumber;
+    }
+
+    public String getRequestDate() {
+        return requestDate;
+    }
+
+    public void setRequestDate(String requestDate) {
+        this.requestDate = requestDate;
+    }
+
+    public Customer getQuoteCustomer() {
+        return quoteCustomer;
+    }
+
+    public void setQuoteCustomer(Customer quoteCustomer) {
+        this.quoteCustomer = quoteCustomer;
+    }
+
+    public PoolDealer getQuoteDealer() {
+        return quoteDealer;
+    }
+
+    public void setQuoteDealer(PoolDealer quoteDealer) {
+        this.quoteDealer = quoteDealer;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Quote quote = (Quote) o;
+        return id == quote.id &&
+                orderNumber == quote.orderNumber &&
+                Objects.equals(requestDate, quote.requestDate) &&
+                Objects.equals(quoteCustomer, quote.quoteCustomer) &&
+                Objects.equals(quoteDealer, quote.quoteDealer);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, orderNumber, requestDate, quoteCustomer, quoteDealer);
+    }
+
+    @Override
+    public String toString() {
+        return "Quote{" +
+                "id=" + id +
+                ", orderNumber=" + orderNumber +
+                ", requestDate='" + requestDate + '\'' +
+                ", quoteCustomer=" + quoteCustomer +
+                ", quoteDealer=" + quoteDealer +
+                '}';
     }
 
 }
