bfec2f6af7a49a84983c128658a15cf3f06fe3d6
Wagenheim <wagenheimk@me.com>
2020-10-19 11:09:32 -0400

    implemented crud for pools

--- src/main/java/com/khkeipe/poolQuote/services/PoolService.java
+++ src/main/java/com/khkeipe/poolQuote/services/PoolService.java
@@ -1,6 +1,7 @@
 package com.khkeipe.poolQuote.services;
 
 import com.khkeipe.poolQuote.entities.Pool;
+import com.khkeipe.poolQuote.exceptions.BadRequestException;
 import com.khkeipe.poolQuote.exceptions.DataPercistanceExecption;
 import com.khkeipe.poolQuote.exceptions.NotFoundException;
 import com.khkeipe.poolQuote.exceptions.ServerErrorException;
@@ -11,8 +12,6 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
-
-//TODO crud
 
 @Service
 public class PoolService {
@@ -53,6 +52,19 @@ public class PoolService {
 
     @Transactional
     public Pool createPool(Pool newPool){
+
+        if (
+                newPool.getId() <= 0 ||
+                newPool.getPoolType().trim().equals("") || newPool.getPoolType() == null ||
+                newPool.getPoolCode().trim().equals("") || newPool.getPoolCode() == null ||
+                newPool.getLength() <= 0 ||
+                newPool.getWidth() <= 0 ||
+                newPool.getHeight() <= 0 ||
+                newPool.getHopperSize() <= 0
+        ){
+            throw new BadRequestException("Please give a valid Pool");
+        }
+
         Pool pool;
 
         try{
@@ -61,6 +73,32 @@ public class PoolService {
             throw new DataPercistanceExecption("New pool was not saved");
         }
         return pool;
+    }
+
+    public Pool updatePool(Pool updatedPool){
+
+        if (
+                updatedPool.getId() <= 0 ||
+                updatedPool.getPoolType().trim().equals("") || updatedPool.getPoolType() == null ||
+                updatedPool.getPoolCode().trim().equals("") || updatedPool.getPoolCode() == null ||
+                updatedPool.getLength() <= 0 ||
+                updatedPool.getWidth() <= 0 ||
+                updatedPool.getHeight() <= 0 ||
+                updatedPool.getHopperSize() <= 0
+        ){
+            throw new BadRequestException("Please give a valid Pool");
+        }
+
+        try{
+            poolRepo.findById(updatedPool.getId()).get();
+        } catch(Exception e){
+            throw new NotFoundException("No Pool found with that ID");
+        }
+
+        poolRepo.save(updatedPool);
+
+        return updatedPool;
+
     }
 
 }
